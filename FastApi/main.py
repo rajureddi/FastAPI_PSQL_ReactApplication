6077c13891dc004b7c152ce209d98dfcbb9d9238
RAJUReDDY <69119292+rajureddi@users.noreply.github.com>
2026-01-21 04:00:02 +0530

    Update main.py

--- FastApi/main.py
+++ FastApi/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # ------------------ CORS ------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],   # DEV only
+    allow_origins=["*"],   
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
